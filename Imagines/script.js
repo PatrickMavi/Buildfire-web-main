a87591d38882b75d27b922a7a8bfe7602848186b
Patrick Mavi <patrickgamu4@gmail.com>
2024-06-20 11:24:36 +0200

    first commit

---
+++ Imagines/script.js
@@ -0,0 +1,19 @@
+const carousel = document.querySelector("");
+
+let isDragStart = false;
+
+const dragStart = () => {
+      isDragStart = true;
+}
+
+const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.scrollLeft = e.pageX;
+
+}
+
+
+
+carousel.addEventListener("mousedown" , dragStart)
+carousel.addEventListener("mousemove" , dragging)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Buildfire-web
